da9afaa4e2c265ec49908f8e47b71474662bd620
efeberktnci <efeberktanci@gmail.com>
2023-12-19 17:51:07 +0300

    Created header for home page

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,21 +4,23 @@ import { AiFillBell, AiFillVideoCamera } from "react-icons/ai"
 
 const Header = () => {
   return ( 
-  <header>
-    <Link>
+  <header className="flex justify-between items-center p-4">
+    <Link className="flex items-center gap-[10px]">
      <img src="/youtube.png" className="w-12" />
-     <h1 className="text-amber">Youtube</h1>
+     <h1 className="text-amber max-sm:hidden">YouTube</h1>
       
     </Link>
 
-    <form>
-        <input type="text-area"/>
-        <button><GoSearch/></button>
+    <form flex className="flex items-center border border-gray-400 rounded-[20px] ">
+        <input type="text" className="bg-black outline-none rounded-[20px] "/>
+        <button className="px-[8px] border-l" >
+          <GoSearch/>
+          </button>
     </form>
 
-    <div>
-        <AiFillBell/>
-        <AiFillVideoCamera/>
+    <div className="flex gap-3 text-xl cursor-pointer">
+        <AiFillBell className="hover:text-gray-400"/>
+        <AiFillVideoCamera className="hover:text-gray-400"/>
         
     </div>
 
